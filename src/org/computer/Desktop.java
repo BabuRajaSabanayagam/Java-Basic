8cb0af8814c220ca184e2492817778996c6f225f
BabuRajaSabanayagam <baburajasabanayagam@outlook.com>
2022-03-05 18:56:12 +0530

    123

--- src/org/computer/Desktop.java
+++ src/org/computer/Desktop.java
@@ -6,6 +6,7 @@ public class Desktop extends Software {
 	public void student() {
 
 		System.out.println("Student Name babu");
+		System.out.println("riyas");
 	}
 
 public static void main(String[] args) {
